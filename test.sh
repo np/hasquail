9fb9c94bcbc93c01d759b8dad60f7a0af390ccca
Fabrizio Biondi <fbio@itu.dk>
2013-05-08 17:38:56 +0200

    added more voting scheme examples

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-#bnfc  quail.cf
-#alex  Lexquail.x
-#happy Parquail.y
-ghc --make '-hide-package monads-tf' -Wall -fno-warn-name-shadowing Run.hs
+bnfc  quail.cf
+alex  Lexquail.x
+happy Parquail.y
+ghc --make  -Wall -fno-warn-name-shadowing Run.hs
 for i; do
   echo "$i"
   ./Run "$i"
